979c6321c42207b981707c19eadd6bc437854b47
María del Rocío Almeida Hernández <ralmher95@gmail.com>
2026-04-21 09:33:32 +0200

    fix: await fetch está suelto fuera de toda función

--- Simulador_de_jubilacion/src/App.jsx
+++ Simulador_de_jubilacion/src/App.jsx
@@ -1,12 +1,8 @@
 import MonteCarloChart from './MonteCarloChart';
 import './index.css'; 
 
-// Busca esta línea y cámbiala:
-const response = await fetch('https://montecarlo-simulator-63u9.onrender.com/api_datos.php');
-
 function App() {
   return (
-
     <main>
       <MonteCarloChart />
     </main>
